0d5ba156c872699d6ae66649c8176113b4a87996
Emily Thomas <eathomas1@ualr.edu>
2023-04-21 21:03:07 -0400

    Custom Nav Layout

--- app/src/main/java/com/example/mypantry/ui/grocery/GroceryViewModel.java
+++ app/src/main/java/com/example/mypantry/ui/grocery/GroceryViewModel.java
@@ -1,16 +1,16 @@
-package com.example.mypantry.ui.dashboard;
+package com.example.mypantry.ui.grocery;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class DashboardViewModel extends ViewModel {
+public class GroceryViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public DashboardViewModel() {
+    public GroceryViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is dashboard fragment");
+        mText.setValue("This is home fragment");
     }
 
     public LiveData<String> getText() {
